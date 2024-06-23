80a7bb662f5c99563669667898e50b73967f75ad
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-06-23 12:27:00 +0530

    backend completed till userRoutes and cookie based authentication

---
+++ backend/src/config/openai-config.ts
@@ -0,0 +1,10 @@
+// import { Configuration } from "openai";
+
+
+// export const configureOpenAI = () => {
+//     const config = new Configuration({
+//         apiKey: process.env.OPEN_AI_SECRET,
+//         organization: process.env.OPENAI_ORGANIZATION_ID
+//     });
+//     return config;
+// }
---
+++ backend/src/controllers/chatController.ts
@@ -0,0 +1,3 @@
+// import { NextFunction, Request, Response } from "express";
+// import { configureOpenAI } from "../config/openai-config";
+// import { OpenAIApi, ChatCompletionRequestMessage } from '@openai/openai';
