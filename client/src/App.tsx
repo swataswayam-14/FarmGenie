3d234938d15b0c00a9dff8592b735af7ea835e57
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-06-23 20:14:44 +0530

    complete frontend logic added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/footer/Footer";
 
 function App() {
-
   return (
-    <div>App.tsx</div>    
-  )
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built With love by
+          <span>
+            <Link
+              style={{ color: "white" }}
+              className="nav-link"
+              to={"https://swataswayamdash.vercel.app"}
+            >
+              Swata Swayam Dash
+            </Link>
+          </span>
+          💘
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
