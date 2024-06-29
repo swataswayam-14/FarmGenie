b67448f15d1e163c411fc5978eb0d2113963f0fa
Turbobot <turbobot@vercel.com>
2024-06-29 09:11:35 +0530

    feat(create-turbo): apply official-starter transform

--- backend/src/controllers/chatController.ts
+++
@@ -1,3 +0,0 @@
-// import { NextFunction, Request, Response } from "express";
-// import { configureOpenAI } from "../config/openai-config";
-// import { OpenAIApi, ChatCompletionRequestMessage } from '@openai/openai';
