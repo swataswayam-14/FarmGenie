7d1dbf0cb8bd72106b5bde93e9851535d75a8996
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-02 10:54:46 +0530

    database initialized and nextAuth implementation logic added

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
 import { Footer } from "./components/Footer";
-import { Appbar } from "./components/AppBar";
 
 export default function Home() {
   return (
    <div>
-    {/* <Appbar/> */}
     <Hero/>
     <Footer/>
    </div>
