fa21825034c77558666267f5e3c9994d86e251ad
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-04 22:37:00 +0530

    react application for farm marketplace added

---
+++ marketplace/client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/NavBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Navbar/>
+  )
+}
+
+export default App
