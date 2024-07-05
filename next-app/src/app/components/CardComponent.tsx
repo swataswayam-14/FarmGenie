1d12840bfadc48aa1839fafe1cecca4231efb060
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-05 13:33:59 +0530

    admin page starting code files added

---
+++ next-app/src/app/components/CardComponent.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface SubjectProps {
+  title: string;
+  subtitle: string;
+  desciption: string;
+}
+
+const CardComponent: React.FC<SubjectProps> = ({ title, subtitle,desciption }) => {
+  return (
+    <div className="bg-purple-100 hover:bg-purple-200 p-6 rounded-lg shadow-md">
+        <h3 className="text-2xl font-bold text-gray-800 mb-4">{title}</h3>
+        <h4 className="text-xl text-gray-600 mb-4">{subtitle}</h4>
+        <p className="text-md text-gray-600 mb-4">{desciption}</p>
+    </div>
+    );
+};
+
+
+export default CardComponent;
