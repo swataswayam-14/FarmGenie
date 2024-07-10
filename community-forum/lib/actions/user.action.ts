d3d7cb3eac6fb3e92a93575682b574ec04f43b2f
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-10 23:21:02 +0530

    contributing content and creating posts functionality added

--- community-forum/lib/actions/user.action.ts
+++ community-forum/lib/actions/user.action.ts
@@ -44,3 +44,15 @@ export async function updateUser({
         throw new Error(`Failed to create/update user: ${error.message}`)
     }
 }
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+        return await User
+            .findOne({
+                id:userId
+            })
+    } catch (error:any) {
+        throw new Error(`Failed to fetch user: ${error.message}`)
+    }
+}
---
+++ community-forum/lib/validations/thread.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const ThreadValidation = z.object({
+    thread: z.string().min(3, {message:'Minimum 3 characters'}),
+    accountId: z.string()
+})
+export const CommentValidation = z.object({
+    thread: z.string().min(3, {message:'Minimum 3 characters'})
+})
