000b4eebfe491c2927ff82d7bdc63d3b5b32525a
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-10 12:51:05 +0530

    onboarding frontend added

---
+++ community-forum/app/api/uploadthing/core.ts
@@ -0,0 +1,35 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { currentUser } from "@clerk/nextjs/server"; 
+
+const f = createUploadthing();
+ 
+const getUser = async () => await currentUser();
+ 
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  media: f({ image: { maxFileSize: "4MB", maxFileCount:1 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = await getUser()
+ 
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError("Unauthorized");
+ 
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+ 
+      console.log("file url", file.url);
+ 
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
