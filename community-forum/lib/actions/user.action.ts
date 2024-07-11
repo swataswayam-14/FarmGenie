41c6fd2ed670aeff6d368c7e80912751fd20251f
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-11 16:43:32 +0530

    profile page created

--- community-forum/lib/actions/user.action.ts
+++ community-forum/lib/actions/user.action.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
+import Thread from "../models/thread.model";
 
 interface Params {
     userId:string;
@@ -54,5 +55,28 @@ export async function fetchUser(userId: string) {
             })
     } catch (error:any) {
         throw new Error(`Failed to fetch user: ${error.message}`)
+    }
+}
+
+export async function fetchUserPosts(userId: string) {
+    try {
+        connectToDB();
+        const threads = await User.findOne({id: userId})
+                .populate({
+                    path:'threads',
+                    model: Thread,
+                    populate:{
+                        path:'children',
+                        model:Thread,
+                        populate:{
+                            path:'author',
+                            model:User,
+                            select:'name image id'
+                        }
+                    }
+                })
+        return threads
+    } catch (error:any) {
+        throw new Error(`failed to fetch posts ${error.message}`)
     }
 }
