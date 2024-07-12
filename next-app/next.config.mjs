eebf4744cbd77ccea92696578ea8ef97dd79e24c
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-12 15:44:31 +0530

    build fixes

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    swcMinify: true,
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
