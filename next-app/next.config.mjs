b9579717dc0b8caca8ba50405f4b55268447d283
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-30 20:53:32 +0530

    recommended product UI added

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    output: 'standalone',
-    experimental: {
-      outputStandalone: true,
-    },
-    api: {
-      bodyParser: false, // Disable body parsing for metrics route
-    },
-  };
-  
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
+  experimental: {
+    outputStandalone: true,
+  },
+  api: {
+    bodyParser: false, 
+  },
+  images: {
+    domains: ['m.media-amazon.com'], 
+  },
+};
+
 export default nextConfig;
